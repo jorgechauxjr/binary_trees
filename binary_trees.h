20002e4ab70e697c3d6ce39bbe5149d58810a814
jorgechauxjr <chauxjorge@hotmail.com>
2020-03-10 18:29:47 +0000

    Add binary tree 6 to 13 and .h file

--- binary_trees.h
+++ binary_trees.h
@@ -33,4 +33,20 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 /*5-Is root*/
 int binary_tree_is_root(const binary_tree_t *node);
+/*6-Pre-order traversal*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/*7-In-order traversal*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+/*8. Post-order traversal*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+/*9. Height*/
+size_t binary_tree_height(const binary_tree_t *tree);
+/*10. Depth*/
+size_t binary_tree_depth(const binary_tree_t *tree);
+/*11.Size*/
+size_t binary_tree_size(const binary_tree_t *tree);
+/*12. Leaves*/
+size_t binary_tree_leaves(const binary_tree_t *tree);
+/*13. Nodes*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
